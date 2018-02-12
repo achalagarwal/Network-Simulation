f3714fbb68bee3c321de76c964f118ac52d79124
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-02-12 18:38:00 +0530

    Manages host connections in a cell site

--- Mobility/Connection.java
+++ Mobility/Connection.java
@@ -1,4 +1,11 @@
 package Mobility;
 
 public class Connection {
+    Host h;
+    double start;
+    public Connection(Host h,double time){
+        this.h = h;
+        this.start = time;
+    }
+
 }

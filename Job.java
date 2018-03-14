e95b972ed9e5570d6ebb1eb78802a77894afd802
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-03-14 21:31:49 +0530

    Added Priority for jobs

--- Job.java
+++ Job.java
@@ -1,18 +1,38 @@
+import java.util.Random;
 public class Job {
     Event event;
     double startTime;
     double endTime;
     int cell;
+    Priority priority;
     Job(int cell, Event event, double startTime){
         this.cell = cell;
         this.event = event;
         this.startTime = startTime;
         this.endTime = -1;
+        generatePriority();
     }
     Job(int cell, Event event, double start, double end){
         this.cell = cell;
         this.event = event;
         this.startTime = start;
         this.endTime = end;
+        generatePriority();
+    }
+    Job(int cell,Event event, double start, double end, Priority p){
+        this.cell = cell;
+        this.event = event;
+        this.startTime = start;
+        this.endTime = end;
+        this.priority = p;
+    }
+    public void generatePriority(){
+        int a = new Random().nextInt(3);
+        if(a == 0)
+            this.priority = Priority.BACKGROUND;
+        else if(a == 1)
+            this.priority = Priority.REALTIME;
+        else
+            this.priority = Priority.STREAMING;
     }
 }

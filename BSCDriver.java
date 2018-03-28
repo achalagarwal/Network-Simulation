8e7800db61483e69c0c5a557a70a126f9ca6deb3
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-03-28 15:06:34 +0530

    Increased sim count to 10

--- BSCDriver.java
+++ BSCDriver.java
@@ -1,22 +1,42 @@
-public class BSCDriver {
-    public static void main(String[] args) {
-        BSC_Control control = new BSC_Control();
-        BSC[] BSCs = new BSC[19];
-        for (int i = 0; i < 19; i++) {
-            BSCs[i] = new BSC(i);
-            control.addBSC(BSCs[i]);
-        }
-        Thread[] threads = new Thread[19];
+import java.util.ArrayList;
 
-        Control con = new Control();
-        con.start();
-        System.out.println("hi");
-        for (BSC c : BSCs) {
-            c.turnOn();
-        }
-        for (int i = 0; i < 19; i++) {
-            threads[i] = new Thread(BSCs[i], Integer.toString(i));
-            threads[i].start();
+public class BSCDriver {
+    public static void reset(){
+        Control.jobs = new ArrayList<>(70);
+        Control.hList = new ArrayList<>(20);
+        Control.time = 0;
+        BSC_Control.bscs = new ArrayList<>();
+    }
+    public static void main(String[] args) {
+        for (int j = 0; j < 10; j++) {
+            System.out.println("SIMULATION "+j+" HAS BEGUN");
+            BSC_Control control = new BSC_Control();
+            BSC[] BSCs = new BSC[19];
+            for (int i = 0; i < 19; i++) {
+                BSCs[i] = new BSC(i);
+                BSCs[i].simNumber = j;
+                control.addBSC(BSCs[i]);
+            }
+            Thread[] threads = new Thread[19];
+
+            Control con = new Control();
+            con.start();
+            System.out.println("hi");
+            for (BSC c : BSCs) {
+                c.turnOn();
+            }
+            for (int i = 0; i < 19; i++) {
+                threads[i] = new Thread(BSCs[i], Integer.toString(i));
+                threads[i].start();
+            }
+            try {
+                threads[0].join();
+                con.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            reset();
+
         }
     }
 }
